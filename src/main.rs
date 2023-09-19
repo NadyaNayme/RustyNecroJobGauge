9a797188e4c0a949022f217ac5f4945be434867c
Nadya <nadya@tfwno.gf>
2023-09-19 11:38:43 -0700

    Thank you Philpax!
    Working but a few more bugs/performance issues to iron out.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![deny(warnings)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::collections::HashMap;
 
-// Don't ask. Remnants of earlier experimentation
 use stoppable_thread;
 
 use simple_websockets::{Event, Responder};
 
-use egui_backend::egui::{ecolor, epaint, ColorImage};
+use egui_backend::egui::{ecolor, epaint};
 use egui_overlay::EguiOverlay;
 use egui_render_three_d::ThreeDBackend;
 use egui_extras::image::RetainedImage;
@@ -18,16 +17,11 @@ fn main()
     run_ws_server()
 }
 
-fn spawn_egui(message: simple_websockets::Message) {
+fn spawn_egui(rx: flume::Receiver<Vec<u8>>) {
     stoppable_thread::spawn(move |_stopped| {
-        let overlay_image_data = match message {
-            simple_websockets::Message::Text(string) => string,
-            simple_websockets::Message::Binary(_) => "Expected String - received Binary".to_string(),
-        };
-        let image: Vec<u8> = image_base64::from_base64(overlay_image_data);
         egui_overlay::start(JobGauge {
             text: "Necromancy Job Gauge".to_string(),
-            overlay: image,
+            rx,
         });
     });
 }
@@ -36,9 +30,10 @@ fn run_ws_server() {
 
     // Setup WebSocket Server
     // listen for WebSockets on port 8080:
-    let event_hub = simple_websockets::launch(8080).expect("failed to listen on port 8080");
+    let event_hub = simple_websockets::launch(8020).expect("failed to listen on port 8020");
     // map between client ids and the client's `Responder`:
     let mut clients: HashMap<u64, Responder> = HashMap::new();
+    let (tx, rx) = flume::unbounded();
 
 
     loop {
@@ -47,6 +42,7 @@ fn run_ws_server() {
                 println!("A client connected with id #{}", client_id);
                 // add their Responder to our `clients` map:
                 clients.insert(client_id, responder);
+                spawn_egui(rx.clone());
             },
             Event::Disconnect(client_id) => {
                 println!("Client #{} disconnected.", client_id);
@@ -60,12 +56,14 @@ fn run_ws_server() {
                 let responder = clients.get(&client_id).unwrap();
                 // echo the message back - this prompts the webapp to see if it needs to send a new frame
                 responder.send(message.clone());
-
-                // I know not to create this every message like I am doing here.
-                // However I don't know how to have it load the image after creation
-                // I want to call this in Event::Connect and have it load images received via Event::Message.
-                // But I don't know how
-                spawn_egui(message);
+                let overlay_image_data = match message {
+                    simple_websockets::Message::Text(string) => string,
+                    simple_websockets::Message::Binary(_) => "Expected String - received Binary".to_string(),
+                };
+                if overlay_image_data.len() > 20 {
+                    let image: Vec<u8> = image_base64::from_base64(overlay_image_data);
+                    let _ = tx.send(image.clone());
+                }
             },
         }
     }
@@ -73,7 +71,7 @@ fn run_ws_server() {
 
 pub struct JobGauge {
     pub text: String,
-    pub overlay: Vec<u8>,
+    pub rx: flume::Receiver<Vec<u8>>,
 }
 
 impl EguiOverlay for JobGauge {
@@ -85,10 +83,10 @@ impl EguiOverlay for JobGauge {
     ) {
         glfw_backend.window.maximize();
 
-        // I want this to be set after init of egui. :(
-        let image = RetainedImage::from_image_bytes("overlay".to_string(), &self.overlay).expect("Failed to load image");
+        let ws_image = self.rx.recv().unwrap();
+        let image = RetainedImage::from_image_bytes("overlay".to_string(), &ws_image).expect("Failed to load image");
 
-        egui_backend::egui::Window::new(&self.text).default_pos((200.0, 200.0)).fixed_size((177.0, 112.0)).title_bar(false).collapsible(false).show(egui_context, |ui| {
+        egui_backend::egui::Window::new(&self.text).default_pos((200.0, 200.0)).default_width(400.0).default_height(400.0).title_bar(false).collapsible(false).show(egui_context, |ui| {
             glfw_backend.window.set_decorated(false);
             glfw_backend.window.set_opacity(1.0);
             let mut style = (*egui_context.style()).clone();
